3c7758f96af182a82f95a1e7943e6d3b2be3d4eb
Tomás Cerveira <trcas@iscte-iul.pt>
2025-12-05 19:44:02 +0000

    Implementação do Form Suite e Relatório Allure (Closes #9)

---
+++ src/test/java/iscteiul/ista/testes_selenium/form_suite/CommunityPage.java
@@ -0,0 +1,38 @@
+package iscteiul.ista.testes_selenium.form_suite;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CommunityPage {
+    // Usamos o Swag Labs porque o site da Vaadin mudou e não tem campos simples acessíveis
+    private static final String URL = "https://www.saucedemo.com/";
+
+    // Seletores (Username e Password)
+    private final SelenideElement usernameField = $("#user-name");
+    private final SelenideElement passwordField = $("#password");
+
+    // Botão de Login
+    private final SelenideElement loginButton = $("#login-button");
+
+    // Elemento que aparece após sucesso (Título da loja)
+    private final SelenideElement successElement = $(".app_logo");
+
+    public void openPage() {
+        open(URL);
+    }
+
+    public void fillForm(String user, String pass) {
+        usernameField.setValue(user);
+        passwordField.setValue(pass);
+    }
+
+    public void submit() {
+        loginButton.click();
+    }
+
+    public SelenideElement getSuccessMessage() {
+        // Retornamos o logo da loja para verificar se o login funcionou
+        return successElement;
+    }
+}
